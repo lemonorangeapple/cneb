7032ac9b8195adb78bbdd89272eee224ad0b20e9
Jiang Chengjun <107529938+lemonorangeapple@users.noreply.github.com>
2023-07-04 09:24:32 +0800

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import *
 import feedparser
 
 rss = feedparser.parse("https://rsshub.app/earthquake/1")
-lst = [{'title': entry['title'].encode('utf8').decode('unicode_escape'), 'description': entry['description'].encode('utf8').decode('unicode_escape')} for entry in rss['entries']]
+lst = [{'title': entry['title'], 'description': entry['description']} for entry in rss['entries']]
 app = Flask(__name__)
 @app.route("/")
 def index():
