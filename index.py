5a140d0e788f57991fe11d701f1584f1b94e5454
Jiang Chengjun <107529938+lemonorangeapple@users.noreply.github.com>
2023-07-04 09:31:49 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -2,8 +2,7 @@ from flask import *
 import feedparser
 
 rss = feedparser.parse("https://rsshub.app/earthquake/1")
-lst = [{'title': entry['title'], 'description': entry['description']} for entry in rss['entries']]
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return lst
+    return rander_template("index.html", rss = rss)
