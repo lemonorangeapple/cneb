8303ea526f3a7f5b9aab7f967bd39a125cc1c739
Jiang Chengjun <107529938+lemonorangeapple@users.noreply.github.com>
2023-09-02 15:39:23 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from flask import *
 import feedparser
 
-rss = feedparser.parse("https://rsshub.app/earthquake/1")
+rss = feedparser.parse("https://rsshub.app/earthquake/ceic/2")
 app = Flask(__name__)
 @app.route("/")
 def index():
