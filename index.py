6fcd1c8000f1c2bc4dec258f91cb5dd4c88c996b
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2023-09-05 19:20:58 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 from flask import *
 import feedparser
 
-rss = feedparser.parse("https://rsshub.app/cneb/yjxx")
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.html", rss = rss)
+    return render_template("index.html", rss = feedparser.parse("https://rsshub.app/cneb/yjxx"))
+
+@app.route("/<path:path>")
+def subpath(path):
+    return render_template("index.html", rss = feedparser.parse(f"https://rsshub.app/cneb/yjxx/{path}"))
